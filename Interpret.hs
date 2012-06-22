6d093539f0263815c31b40e32f51c622128ccb31
Alexandr Alexeev <afiskon@gmail.com>
2012-06-22 09:04:20 +0400

    Interpret.hs

---
+++ Interpret.hs
@@ -0,0 +1,28 @@
+module Interpret (
+    Scalar,
+    Context,
+    createContext,
+    getValue,
+    isError,
+    eval
+  ) where
+import Synt
+import Data.Map as M
+
+data Scalar = SInt Int | SReal Double
+  deriving (Eq, Ord, Show)
+
+data Context = Context {
+    variables :: M.Map String Scalar,
+    isError ::  Bool 
+  }
+  deriving (Eq, Show)
+
+createContext :: Context
+createContext = Context { variables = M.empty, isError = False }
+
+getValue :: Context -> String -> Maybe Scalar
+getValue ctx name = M.lookup name $ variables ctx
+
+eval :: Context -> Synt.Expr -> Context
+eval ctx (Expr varName operator expr) = ctx
