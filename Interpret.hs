dccad5f1bad15700b1bbda0d0d44faa8179dbe76
Alexandr Alexeev <afiskon@gmail.com>
2012-06-22 15:00:51 +0400

    evalRVal (very dirty code...)

--- Interpret.hs
+++ Interpret.hs
@@ -39,11 +39,29 @@ evalExpr ctx (Expr vname rval) =
     Right value -> Right $ ctx { variables = M.insert vname value $ variables ctx }
     Left err -> Left err
 
+scalarToRVal :: Scalar -> Synt.RVal
+scalarToRVal (SInt x) = IntVal x
+scalarToRVal (SReal x) = RealVal x
+
 evalRVal :: Context -> Synt.RVal -> Either String Scalar
 
 evalRVal ctx (IdentVal ident) = 
   case getValue ctx ident of
     Just rval -> Right rval
     Nothing -> Left $ "Undefined variable '" ++ ident ++ "'"
+
+
+evalRVal ctx (Add (IntVal x) (RealVal y)) = evalRVal ctx (Add (RealVal $ fromIntegral x) (RealVal y))
+evalRVal ctx (Add (RealVal x) (IntVal y)) = evalRVal ctx (Add (RealVal x) (RealVal $ fromIntegral y))
+evalRVal _ (Add (IntVal x) (IntVal y)) = Right $ SInt (x + y)
+evalRVal _ (Add (RealVal x) (RealVal y)) = Right $ SReal (x + y)
+evalRVal ctx (Add a b) = 
+  case (evalRVal ctx a, evalRVal ctx b) of
+    (Right x, Right y) -> evalRVal ctx (Add (scalarToRVal x) (scalarToRVal y))
+    (Right x, err) -> err
+    (err, _) -> err
+
+evalRVal _ (IntVal x) = Right $ SInt x
+evalRVal _ (RealVal x) = Right $ SReal x
 
 evalRVal ctx rval = Right $ SInt 0
