62b0a165d8e9951b827172bf82e80d52b06ebd6e
PuM <barbos@inbox.ru>
2020-03-09 15:11:58 +0100

    Import into GitHub

--- LexMain.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-import Lex
-
-main = do
-  s <- getContents
-  putStrLn $ case alexScanTokens s of
-    Right lst -> concatMap (\t -> "'" ++ show t ++ "' ") lst
-    Left err -> "ERROR: " ++ err
